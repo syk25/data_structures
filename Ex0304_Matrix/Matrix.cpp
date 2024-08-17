ade3ae5259f78192339f7f7054f5c0a4e1eb8ad0
syk25 <syk2245@gmail.com>
2024-08-17 20:43:39 +0900

    1차원 배열을 활용한 행렬 구현

--- Ex0304_Matrix/Matrix.cpp
+++ Ex0304_Matrix/Matrix.cpp
@@ -7,33 +7,56 @@
 using namespace std;
 
 Matrix::Matrix(int num_rows, int num_cols) {
-    // TODO:
+    // REVIEW:
+    num_rows_ = num_rows;
+    num_cols_ = num_cols;
+    values_ = new float[num_rows_ * num_cols_];
 }
 
 // 복사 생성자 (b를 복사)
 Matrix::Matrix(const Matrix &b) {
-    // TODO:
+    // REVIEW:
+    num_rows_ = b.num_rows_;
+    num_cols_ = b.num_cols_;
+
+    this->values_ = new float[num_rows_ * num_cols_];
+
+    for (int r = 0; r < num_rows_; r++) {
+        for (int c = 0; c < num_cols_; c++) {
+            int idx = c + num_cols_ * r - 1;
+            this->values_[idx] = b.values_[idx];
+        }
+    }
 }
 
 Matrix::~Matrix() {
-    // TODO:
+    // REVIEW:
+    delete[] this->values_; // NOTE: 문법의 의미 정리하기 -> 제대로 안하면 segmentation fault 발생
 }
 
 void Matrix::SetValue(int row, int col, float value) {
     // values_[TODO] = value;
+    int idx = col + row * this->num_cols_ - 1;
+    this->values_[idx] = value;
 }
 
-float Matrix::GetValue(int row, int col)
-    const // 맨 뒤의 const는 함수 안에서 멤버 변수의 값을 바꾸지 않겠다는 의미
+float Matrix::GetValue(int row, int col) const // 맨 뒤의 const는 함수 안에서 멤버 변수의 값을 바꾸지 않겠다는 의미
 {
-    return 0.0f;
+    int idx = col + row * this->num_cols_ - 1;
+    return this->values_[idx];
 }
 
 Matrix Matrix::Transpose() {
     Matrix temp(num_cols_, num_rows_); // num_cols_, num_rows_ 순서 주의
 
     // TODO:
-
+    for (int r = 0; r < num_rows_; r++) {
+        for (int c = 0; c < num_cols_; c++) {
+            int temp_idx = r + c * num_rows_ - 1;
+            int this_idx = c + r * num_cols_ - 1;
+            temp.values_[temp_idx] = this->values_[this_idx];
+        }
+    }
     return temp;
 }
 
@@ -43,8 +66,13 @@ Matrix Matrix::Add(const Matrix &b) {
 
     Matrix temp(num_rows_, num_cols_);
 
-    // TODO:
-
+    // REVIEW:
+    for (int r = 0; r < num_rows_; r++) {
+        for (int c = 0; c < num_cols_; c++) {
+            int idx = c + num_cols_ * r - 1;
+            temp.values_[idx] = this->values_[idx] + b.values_[idx];
+        }
+    }
     return temp;
 }
 
