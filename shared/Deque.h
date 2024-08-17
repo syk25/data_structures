adc70ae78288573aa80f623ba2479b28dc0b3191
syk25 <syk2245@gmail.com>
2024-08-18 08:34:37 +0900

    style: 공용코드 포맷팅

--- shared/Deque.h
+++ shared/Deque.h
@@ -3,57 +3,38 @@
 #include "Queue.h"
 
 #include <cassert>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
 // Double Ended Queue (덱, dequeue와 구분)
-template<typename T>
-class Deque : public Queue<T>
-{
+template <typename T> class Deque : public Queue<T> {
 
-	typedef Queue<T> Base;
+    typedef Queue<T> Base;
 
-public:
-	Deque(int capacity)
-		: Queue<T>(capacity)
-	{
-	}
+  public:
+    Deque(int capacity) : Queue<T>(capacity) {}
 
-	T& Front()
-	{
-		return Base::Front();
-	}
+    T &Front() { return Base::Front(); }
 
-	T& Back()
-	{
-		return Base::Rear();
-	}
+    T &Back() { return Base::Rear(); }
 
-	void PushFront(const T& item)
-	{
-		if (Base::IsFull())
-			Base::Resize();
+    void PushFront(const T &item) {
+        if (Base::IsFull())
+            Base::Resize();
 
-		// TODO:
-	}
+        // TODO:
+    }
 
-	void PushBack(const T& item)
-	{
-		Base::Enqueue(item);
-	}
+    void PushBack(const T &item) { Base::Enqueue(item); }
 
-	void PopFront()
-	{
-		Base::Dequeue();
-	}
+    void PopFront() { Base::Dequeue(); }
 
-	void PopBack()
-	{
-		assert(!Base::IsEmpty());
+    void PopBack() {
+        assert(!Base::IsEmpty());
 
-		// TODO:
-	}
+        // TODO:
+    }
 
-private:
-	// Queue와 동일
+  private:
+    // Queue와 동일
 };
--- shared/DoublyLinkedList.h
+++ shared/DoublyLinkedList.h
@@ -1,149 +1,123 @@
 ﻿#pragma once
 
+#include <algorithm>
 #include <cassert>
 #include <stdint.h>
-#include <algorithm>
 
-template<typename T>
-class DoublyLinkedList
-{
-public:
-	struct Node
-	{
-		T item = T();
+template <typename T> class DoublyLinkedList {
+  public:
+    struct Node {
+        T item = T();
 
-		Node* left = nullptr;
-		Node* right = nullptr;
+        Node *left = nullptr;
+        Node *right = nullptr;
 
-		// 참고: prev/next가 아니라 left/right
-	};
+        // 참고: prev/next가 아니라 left/right
+    };
 
-	DoublyLinkedList()
-	{
-	}
+    DoublyLinkedList() {}
 
-	~DoublyLinkedList()
-	{
-		Clear();
-	}
+    ~DoublyLinkedList() { Clear(); }
 
-	void Clear() // 모두 지워야(delete) 합니다.
-	{
-		// TODO:
-	}
+    void Clear() // 모두 지워야(delete) 합니다.
+    {
+        // TODO:
+    }
 
-	bool IsEmpty()
-	{
-		return true; // TODO:
-	}
+    bool IsEmpty() {
+        return true; // TODO:
+    }
 
-	int Size()
-	{
-		int size = 0;
+    int Size() {
+        int size = 0;
 
-		// TODO:
+        // TODO:
 
-		return size;
-	}
+        return size;
+    }
 
-	void Print()
-	{
-		using namespace std;
+    void Print() {
+        using namespace std;
 
-		Node* current = first_;
+        Node *current = first_;
 
-		if (IsEmpty())
-			cout << "Empty" << endl;
-		else
-		{
-			cout << "Size = " << Size() << endl;
+        if (IsEmpty())
+            cout << "Empty" << endl;
+        else {
+            cout << "Size = " << Size() << endl;
 
-			cout << " Forward: ";
-			// TODO:
-			cout << endl;
+            cout << " Forward: ";
+            // TODO:
+            cout << endl;
 
-			cout << "Backward: ";
-			// TODO:
-			cout << endl;
-		}
-	}
+            cout << "Backward: ";
+            // TODO:
+            cout << endl;
+        }
+    }
 
-	Node* Find(T item)
-	{
-		return nullptr; // TODO:
-	}
+    Node *Find(T item) {
+        return nullptr; // TODO:
+    }
 
-	void InsertBack(Node* node, T item)
-	{
-		if (IsEmpty())
-		{
-			PushBack(item);
-		}
-		else
-		{
-			// TODO:
-		}
-	}
+    void InsertBack(Node *node, T item) {
+        if (IsEmpty()) {
+            PushBack(item);
+        } else {
+            // TODO:
+        }
+    }
 
-	void PushFront(T item)
-	{
-		// TODO:
-	}
+    void PushFront(T item) {
+        // TODO:
+    }
 
-	void PushBack(T item)
-	{
-		// TODO:
-	}
+    void PushBack(T item) {
+        // TODO:
+    }
 
-	void PopFront()
-	{
-		if (IsEmpty())
-		{
-			using namespace std;
-			cout << "Nothing to Pop in PopFront()" << endl;
-			return;
-		}
+    void PopFront() {
+        if (IsEmpty()) {
+            using namespace std;
+            cout << "Nothing to Pop in PopFront()" << endl;
+            return;
+        }
 
-		assert(first_);
+        assert(first_);
 
-		// TODO:
-	}
+        // TODO:
+    }
 
-	void PopBack()
-	{
-		if (IsEmpty())
-		{
-			using namespace std;
-			cout << "Nothing to Pop in PopBack()" << endl;
-			return;
-		}
+    void PopBack() {
+        if (IsEmpty()) {
+            using namespace std;
+            cout << "Nothing to Pop in PopBack()" << endl;
+            return;
+        }
 
-		// 맨 뒤에서 하나 앞의 노드를 찾아야 합니다.
+        // 맨 뒤에서 하나 앞의 노드를 찾아야 합니다.
 
-		assert(first_);
+        assert(first_);
 
-		// TODO:
-	}
+        // TODO:
+    }
 
-	void Reverse()
-	{
-		// TODO:
-	}
+    void Reverse() {
+        // TODO:
+    }
 
-	T Front()
-	{
-		assert(first_);
+    T Front() {
+        assert(first_);
 
-		return T(); // TODO:
-	}
+        return T(); // TODO:
+    }
 
-	T Back()
-	{
-		assert(first_);
+    T Back() {
+        assert(first_);
 
-		return T(); // TODO:
-	}
+        return T(); // TODO:
+    }
 
-protected:
-	Node* first_ = nullptr;
+  protected:
+    Node *first_ = nullptr;
 };
