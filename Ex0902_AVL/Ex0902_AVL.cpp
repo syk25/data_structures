d537178cd79c87f7e7a583fea5a37d9e5d2fc45f
syk25 <syk2245@gmail.com>
2024-09-19 04:50:10 +0900

    [format] 포맷팅

--- Ex0902_AVL/Ex0902_AVL.cpp
+++ Ex0902_AVL/Ex0902_AVL.cpp
@@ -4,32 +4,30 @@
 
 using namespace std;
 
-int main()
-{
-	using Item = AVL<int, char>::Item;
+int main() {
+    using Item = AVL<int, char>::Item;
 
-	AVL<int, char> bst;
+    AVL<int, char> bst;
 
-	for (int i : { 9, 8, 7, 6, 5, 4, 3, 2, 1 })
-		//for (int i : { 1, 2, 3, 4, 5, 6, 7, 8, 9 })
-	{
-		bst.Insert(Item{ i, char('A' + i) });
-		bst.Print2D();
-	}
+    for (int i : {9, 8, 7, 6, 5, 4, 3, 2, 1})
+    // for (int i : { 1, 2, 3, 4, 5, 6, 7, 8, 9 })
+    {
+        bst.Insert(Item{i, char('A' + i)});
+        bst.Print2D();
+    }
 
-	bst.Inorder(); // key에 대해 정렬
+    bst.Inorder(); // key에 대해 정렬
 
-	cout << bst.RecurGet(5)->value << endl;
-	cout << bst.RecurGet(3)->value << endl;
-	cout << bst.RecurGet(7)->value << endl;
-	cout << bst.RecurGet(1)->value << endl;
-	cout << bst.RecurGet(4)->value << endl;
+    cout << bst.RecurGet(5)->value << endl;
+    cout << bst.RecurGet(3)->value << endl;
+    cout << bst.RecurGet(7)->value << endl;
+    cout << bst.RecurGet(1)->value << endl;
+    cout << bst.RecurGet(4)->value << endl;
 
-	for (int i : {5, 4, 7, 8, 9})
-	{
-		bst.Remove(i);
-		bst.Print2D();
-	}
+    for (int i : {5, 4, 7, 8, 9}) {
+        bst.Remove(i);
+        bst.Print2D();
+    }
 
-	return 0;
+    return 0;
 }
