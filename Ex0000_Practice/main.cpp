5d3af0b3b20ebe40e5ff229b96847da1d1eff11f
syk25 <syk2245@gmail.com>
2024-09-29 21:40:18 +0900

    # 1.1 swap - 로직을 머리 속으로 그리기 - 모든 코드는 동작을 의미 - 코드의 가독성을 높이기 위해서는 함수 정의 활용하기 - 정렬 알고리즘에서 자리교체를 위해 swap 값 비교후 swap 로직 사용

--- Ex0000_Practice/main.cpp
+++ Ex0000_Practice/main.cpp
@@ -1,4 +1,4 @@
-/* 
+/*
 swap 연습
 1. 포인터를 이용한 swap 구현
 2. 레퍼런스를 이용한 swap 구현
@@ -10,19 +10,24 @@ swap 연습
 
 using namespace std;
 
-void swapPtr(int *a, int *b){
+void swapPtr(int *a, int *b) {
+    cout << "Before| Swap by Pointer: " << "a: " << a << ", b: " << b << endl;
     int temp = *a;
     *a = *b;
     *b = temp;
+
+    cout << "After | Swap by Pointer: " << "a: " << a << ", b: " << b << endl;
 }
 
-void swapRef(int &a, int &b){
+void swapRef(int &a, int &b) {
+    cout << "Before| Swap by Reference: " << "a: " << a << ", b: " << b << endl;
     int temp = a;
     a = b;
     b = temp;
+    cout << "After | Swap by Reference: " << "a: " << a << ", b: " << b << endl;
 }
 
-int main(){
+int main() {
     int a = 10;
     int b = 20;
 
@@ -32,10 +37,10 @@ int main(){
     // swapPtr(&a, &b);
 
     // 2. 레퍼런스를 이용한 swap 구현
-    // swapRef(a, b);
+    swapRef(a, b);
 
     // 3. 표준함수를 이용한 swap 구현
-    swap(a, b);
+    // swap(a, b);
 
     cout << "after swap: a: " << a << ", b: " << b << endl;
 
--- Ex0101_Swap/Ex0101_Swap.cpp
+++ Ex0101_Swap/Ex0101_Swap.cpp
@@ -2,63 +2,74 @@
 
 using namespace std;
 
-/*
-int MySwapValue(int i, int j)
-{
-	// TODO:
+/* int MySwapValue(int i, int j) {
+    // NOTE: call by value - 값을 복사해옴 + 완전 새로운 변수를 만듬 + 반환을 하기가 난감함
+    int temp = i;
+    i = j;
+    j = temp;
+    return -1;
+} */
 
-	return ? ;
+void MySwapPtr(int *i, int *j) {
+    // NOTE: 포인터로 매개변수를 받는 경우
+    int temp = *i;
+    *i = *j;
+    *j = temp;
 }
 
-void MySwapPtr(int* i, int* j)
-{
-	// TODO:
+void MySwapRef(int &i, int &j) {
+    // NOTE: Reference로 매개변수를 받는 경우 - 변수 자체에 이름을 하나 더 붙인 경우 - 객체를 복사하지 않음
+    int temp = i;
+    i = j;
+    j = temp;
 }
 
-void MySwapRef(int& i, int& j)
-{
-	// TODO:
-}
-*/
+bool CheckSorted(int a, int b) { return !(a > b); }
 
-bool CheckSorted(int a, int b)
-{
-	return false;
-}
+int main() {
+    // Swap
+    {
+        int a = 3;
+        int b = 2;
 
-int main()
-{
-	// Swap
-	{
-		int a = 3;
-		int b = 2;
+        cout << "Before: " << a << " " << b << endl;
 
-		cout << a << " " << b << endl;
+        // COMPLETE:
+        // MySwapPtr(&a, &b);
+        MySwapRef(a, b);
 
-		// TODO:
+        cout << "After : " << a << " " << b << endl;
+    }
 
-		cout << a << " " << b << endl;
-	}
+    // 정렬(sorting)
+    {
+        int arr[] = {9, 3};
 
+        cout << arr[0] << " " << arr[1] << endl;
 
-	// 정렬(sorting)
-	{
-		int arr[] = { 9, 3 };
+        // COMPLETE:
+        if (arr[0] > arr[1])
+            MySwapRef(arr[0], arr[1]);
 
-		cout << arr[0] << " " << arr[1] << endl;
+        cout << arr[0] << " " << arr[1] << endl;
+    }
 
-		// TODO:
+    // 값과 상관 없이 항상 작은 값이 먼저 출력되게 하려면?
+    // 두 값이 같을 때는 순서가 상관 없음 -> 큰 값이 먼저 출력되지 않게 하려면?
+    {
+        int arr[2];
 
-		cout << arr[0] << " " << arr[1] << endl;
-	}
+        // COMPLETE:
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                arr[0] = i;
+                arr[1] = j;
+                cout << boolalpha;
+                cout << i << " " << j;
+                cout << " " << (arr[0] <= arr[1]) << endl;
+            }
+        }
+    }
 
-	// 값과 상관 없이 항상 작은 값이 먼저 출력되게 하려면?
-	// 두 값이 같을 때는 순서가 상관 없음 -> 큰 값이 먼저 출력되지 않게 하려면?
-	{
-		int arr[2];
-
-		// TODO:
-	}
-
-	return 0;
+    return 0;
 }
