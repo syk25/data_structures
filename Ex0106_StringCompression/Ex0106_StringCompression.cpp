a87f9a0532eb1b6c7f85ef891e5442c62bae1766
syk25 <syk2245@gmail.com>
2024-09-30 03:07:25 +0900

    # 1.6 문자열 압축

--- Ex0106_StringCompression/Ex0106_StringCompression.cpp
+++ Ex0106_StringCompression/Ex0106_StringCompression.cpp
@@ -1,110 +1,128 @@
-﻿#include <iostream>
-#include <cassert>
+﻿#include <cassert>
+#include <iostream>
 
 using namespace std;
 
-void InsertionSort(char* arr, int n)
-{
-	int i, key, j;
-	for (i = 1; i < n; i++) {
-		key = arr[i];
-		j = i - 1;
+void InsertionSort(char *arr, int n) {
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
 
-		while (j >= 0 && arr[j] > key) // <- while 사용
-		{
-			arr[j + 1] = arr[j];
-			j = j - 1;
-		}
+        while (j >= 0 && arr[j] > key) // <- while 사용
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
 
-		arr[j + 1] = key;
-	}
+        arr[j + 1] = key;
+    }
 }
 
-int Count(char* arr, int n, char x)
-{
-	int count = 0;
-	for (int i = 0; i < n; i++) // start index 사용
-	{
-		if (arr[i] == x)
-			count++;
-	}
+int Count(char *arr, int n, char x) {
+    int count = 0;
+    for (int i = 0; i < n; i++) // start index 사용
+    {
+        if (arr[i] == x)
+            count++;
+    }
 
-	return count;
+    return count;
 }
 
-int main()
-{
-	// 문제
-	// - 어떤 알파벳에 몇 번 나오는지로 출력
-	// - 예시) aaabbccdddeeeff -> a3b2c2d3e3f2
-	// - 알파벳은 총 26 글자	
+void Print(int *arr, int n) {
+    for (int i = 0; i < n; i++) {
+        cout << i << ": " << arr[i] << endl;
+    }
+}
 
-	// cout << int('a') << " " << int('z') << endl; // 97 122
+void print_letter(char *arr, int n, int i) {
+    int count = Count(arr, n, char(i + 'a'));
+    if (count != 0) {
+        cout << char(i + 'a') << count;
+    }
+}
 
-	// char arr[] = "ababcdfdceeefda";
-	// char arr[] = "a";
-	char arr[] = "ababcdfdceeedag";
-	int n = sizeof(arr) - 1; // 마지막 안보이는 '\0' 제외
+int main() {
+    // 문제
+    // - 어떤 알파벳에 몇 번 나오는지로 출력
+    // - 예시) aaabbccdddeeeff -> a3b2c2d3e3f2
+    // - 알파벳은 총 26 글자
 
-	// 글자가 하나이상이라고 가정
-	// 글자가 없으면 if로 아예 수행을 안하도록 처리 가능
-	assert(n >= 1);
+    // cout << int('a') << " " << int('z') << endl; // 97 122
 
-	cout << arr << endl;
+    // char arr[] = "ababcdfdceeefda";
+    // char arr[] = "a";
+    char arr[] = "ababcdfdceeedag";
+    int n = sizeof(arr) - 1; // 마지막 안보이는 '\0' 제외
 
-	// 풀이 1. 모든 알파벳에 대해서 Count()
-	// 힌트: 소문자 알파벳 'a'~'z'는 int로는 97~122에 대응
-	// 단점: 없는 알파벳도 세야 한다.
+    // 글자가 하나이상이라고 가정
+    // 글자가 없으면 if로 아예 수행을 안하도록 처리 가능
+    assert(n >= 1);
 
-	// 표를 사용할 수도 있고 사용하지 않을 수도 있음
-	int table[26] = { 0 }; // 모든 값을 0으로 초기화
+    cout << arr << endl;
 
-	for (int i = 0; i < 26; i++)
-	{
-		// 힌트: char(i + 97)
+    // 풀이 1. 모든 알파벳에 대해서 Count()
+    // 힌트: 소문자 알파벳 'a'~'z'는 int로는 97~122에 대응
+    // 단점: 없는 알파벳도 세야 한다.
 
-		// 표를 만들고 나중에 몰아서 출력하는 방법
-		// table[i] = ...
+    // 표를 사용할 수도 있고 사용하지 않을 수도 있음
+    int table[26] = {0}; // 모든 값을 0으로 초기화
 
-		// 표를 만들지 않고 직접 출력하는 방법
-		// ...
-	}
+    for (int i = 0; i < 26; i++) {
+        // 힌트: char(i + 97)
 
-	cout << endl;
+        // 표를 만들고 나중에 몰아서 출력하는 방법
+        // table[i] = ...
 
-	// 출력
-	for (int i = 0; i < 26; i++)
-	{
-		// ...
-	}
-	cout << endl << endl;
+        // 표를 만들지 않고 직접 출력하는 방법
+        // ...
 
-	// 풀이 2. 정렬 후 찾기
-	// 여기서는 별도의 문자열을 만드는 것이 아니라 출력이 목표
-	// Table도 만들지 않음
+        // NOTE: 표를 사용하는 경우
+        table[i] = Count(arr, n, char(i + 'a'));
 
-	InsertionSort(arr, n);
+        // NOTE: 직접 출력하는 방법
+        // print_letter(arr, n, i);
+    }
+    // Print(table, sizeof(table) / sizeof(int));
 
-	cout << arr << endl;
+    cout << endl;
 
-	char c = arr[0];
-	int count = 1;
+    // 출력
+    for (int i = 0; i < 26; i++) {
+        if (table[i] != 0) {
+            cout << char(i + 'a');
+            cout << table[i];
+        }
+    }
+    cout << endl << endl;
 
-	cout << c;
+    // 풀이 2. 정렬 후 찾기
+    // 여기서는 별도의 문자열을 만드는 것이 아니라 출력이 목표
+    // Table도 만들지 않음
 
-	for (int i = 1; i < n; i++)
-	{
-		if (arr[i] == c)
-		{
-			// TODO: ...
-		}
-		else
-		{
-			// TODO: ...
-		}
-	}
+    InsertionSort(arr, n);
 
-	cout << count << endl; // 마지막 count 출력
+    cout << arr << endl;
 
-	return 0;
+    char c = arr[0];
+    int count = 1;
+
+    cout << c;
+
+    // COMPLETE:
+    for (int i = 1; i < n; i++) {
+        if (arr[i] == c) {
+            count++;
+        } else {
+            cout << count;
+            count = 1;
+            c = arr[i];
+            cout << c;
+        }
+    }
+
+    cout << count << endl; // 마지막 count 출력
+
+    return 0;
 }
