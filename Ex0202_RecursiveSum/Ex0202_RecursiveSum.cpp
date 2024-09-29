e1cd4368c7fb5b0936694db7a186263aedec641e
syk25 <syk2245@gmail.com>
2024-09-30 03:56:29 +0900

    # 2.2 재귀호출을 활용한 합 구하기

--- Ex0202_RecursiveSum/Ex0202_RecursiveSum.cpp
+++ Ex0202_RecursiveSum/Ex0202_RecursiveSum.cpp
@@ -1,40 +1,41 @@
-﻿#include <iostream>
+﻿#include <algorithm> // swap
 #include <cassert>
-#include <algorithm> // swap
+#include <iostream>
 
 using namespace std;
 
-int Sum(int* arr, int n)
-{
-	int sum = 0;
+int Sum(int *arr, int n) {
+    int sum = 0;
 
-	for (int i = 0; i < n; i++)
-		sum += arr[i];
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
 
-	return sum;
+    return sum;
 }
 
-int RecurSum(int* arr, int n)
-{
-	/* 힌트: 역순으로 더하기 (매개변수에 시작 인덱스를 추가하지 않아도 되기 때문)
-	  (1 + 2 + ... 8 + 9 + 10)
-	= (1 + 2 + ... 8 + 9) + 10
-	= ((1 + 2 + ... 8) + 9) + 10
-	= ...
-	*/
+int RecurSum(int *arr, int n) {
+    /* 힌트: 역순으로 더하기 (매개변수에 시작 인덱스를 추가하지 않아도 되기 때문)
+      (1 + 2 + ... 8 + 9 + 10)
+    = (1 + 2 + ... 8 + 9) + 10
+    = ((1 + 2 + ... 8) + 9) + 10
+    = ...
+    */
 
-	return 0; // <- TODO: 
+    if (n <= 0) {
+        return 0;
+    }
+    return RecurSum(arr, n - 1) + arr[n - 1];
+    cout << n << " ";
 }
 
-int main()
-{
-	// Sum vs RecurSum
+int main() {
+    // Sum vs RecurSum
 
-	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int n = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-	cout << Sum(arr, n) << endl;
-	cout << RecurSum(arr, n) << endl;
+    cout << Sum(arr, n) << endl;
+    cout << RecurSum(arr, n) << endl;
 
-	return 0;
+    return 0;
 }
