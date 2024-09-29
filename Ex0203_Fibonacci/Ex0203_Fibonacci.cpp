810874db8147d85f96052a49ccc215d7a2cfbe99
syk25 <syk2245@gmail.com>
2024-09-30 04:15:48 +0900

    # 2.3 피보나치수열

--- Ex0203_Fibonacci/Ex0203_Fibonacci.cpp
+++ Ex0203_Fibonacci/Ex0203_Fibonacci.cpp
@@ -1,52 +1,61 @@
-﻿#include <iostream>
+﻿#include <algorithm> // swap
 #include <cassert>
-#include <algorithm> // swap
 #include <iomanip> // setw
+#include <iostream>
 
 using namespace std;
 
-int Fibonacci(int n)
-{
-	if (n == 0)
-		return 0; // F0 = 0 
-	else if (n == 1)
-		return 1; // F1 = 1
-	else
-	{
-		int fn = 0;
+int Fibonacci(int n) {
+    if (n == 0)
+        return 0; // F0 = 0
+    else if (n == 1)
+        return 1; // F1 = 1
+    else {
+        int fn = 0;
 
-		// TODO:
+        // TODO:
+        int a = 0;
+        int b = 1;
+        for (int i = 2; i <= n; i++) {
+            fn = a + b;
+            a = b;
+            b = fn;
+        }
 
-		return fn;
-	}
+        return fn;
+    }
 }
 
-int RecurFibonacci(int n)
-{
-	return 0;
+int RecurFibonacci(int n) {
+    if (n == 0) {
+        return 0;
+    } else if (n == 1) {
+        return 1;
+    } else {
+        return RecurFibonacci(n - 1) + RecurFibonacci(n - 2);
+    }
 }
 
-int main()
-{
-	// Input      : 0   1   2   3   4   5   6   7   8   9  10  11  12  13  14
-	// Fibonnacci : 0   1   1   2   3   5   8  13  21  34  55  89 144 233 377
-	// Fibonnacci : 0   1   1   2   3   5   8  13  21  34  55  89 144 233 377
+int main() {
+    // Input      : 0   1   2   3   4   5   6   7   8   9  10  11  12  13  14
+    // Fibonnacci : 0   1   1   2   3   5   8  13  21  34  55  89 144 233 377
+    // Fibonnacci : 0   1   1   2   3   5   8  13  21  34  55  89 144 233 377
 
-	cout << "Input     : ";
-	for (int i = 0; i < 15; i++)
-		cout << setw(3) << i << " ";
-	cout << endl;
+    cout << "Input     : ";
+    for (int i = 0; i < 15; i++)
+        cout << setw(3) << i << " ";
+    cout << endl;
 
-	// Fibonnacci
-	cout << "Fibonnacci: ";
-	for (int i = 0; i < 15; i++)
-		cout << setw(3) << Fibonacci(i) << " ";
-	cout << endl;
+    // Fibonnacci
+    cout << "Fibonnacci: ";
+    for (int i = 0; i < 15; i++)
+        cout << setw(3) << Fibonacci(i) << " ";
+    cout << endl;
 
-	cout << "Fibonnacci: ";
-	for (int i = 0; i < 15; i++)
-		cout << setw(3) << RecurFibonacci(i) << " ";
-	cout << endl;
+    cout << "Fibonnacci: ";
+    for (int i = 0; i < 15; i++)
+        cout << setw(3) << RecurFibonacci(i) << " ";
+    cout << endl;
 
-	return 0;
+    return 0;
 }
