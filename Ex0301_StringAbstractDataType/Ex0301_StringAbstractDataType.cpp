c3583606aebd08f24adb2f49dab73b9689ed945e
syk25 <syk2245@gmail.com>
2024-09-30 20:44:21 +0900

    # 3.1 문자열 자료형 구현

--- Ex0301_StringAbstractDataType/Ex0301_StringAbstractDataType.cpp
+++ Ex0301_StringAbstractDataType/Ex0301_StringAbstractDataType.cpp
@@ -2,68 +2,66 @@
 
 using namespace std;
 
-int main()
-{
-	// 생성자, MyString::Print()
-	{
-		MyString str1("hi hay he hel hello llo ello el el o!");
-		str1.Print();
-	}
+int main() {
+    // 생성자, MyString::Print()
+    {
+        MyString str1("hi hay he hel hello llo ello el el o!");
+        str1.Print();
+    }
 
-	// Find()
-	{
-		MyString str1("hi hay he hel hello llo ello el el o!");
-		cout << str1.Find(MyString("hell")) << endl;
+    // Find()
+    {
+        MyString str1("hi hay he hel hello llo ello el el o!");
+        cout << str1.Find(MyString("hell")) << endl; // 14
 
-		cout << "Found at " << MyString("ABCDEF").Find(MyString("A")) << endl;
-		cout << "Found at " << MyString("ABCDEF").Find(MyString("AB")) << endl;
-		cout << "Found at " << MyString("ABCDEF").Find(MyString("CDE")) << endl;
-		cout << "Found at " << MyString("ABCDEF").Find(MyString("EF")) << endl;
-		cout << "Found at " << MyString("ABCDEF").Find(MyString("EFG")) << endl;
-		cout << "Found at " << MyString("ABCDEF").Find(MyString("EFGHIJ")) << endl;
-	}
+        cout << "Found at " << MyString("ABCDEF").Find(MyString("A")) << endl;      // 0
+        cout << "Found at " << MyString("ABCDEF").Find(MyString("AB")) << endl;     // 0
+        cout << "Found at " << MyString("ABCDEF").Find(MyString("CDE")) << endl;    // 2
+        cout << "Found at " << MyString("ABCDEF").Find(MyString("EF")) << endl;     // 4
+        cout << "Found at " << MyString("ABCDEF").Find(MyString("EFG")) << endl;    // -1
+        cout << "Found at " << MyString("ABCDEF").Find(MyString("EFGHIJ")) << endl; // -1
+    }
 
-	// 복사 생성자
-	{
-		MyString str1("hi hay he hel hello llo ello el el o!");
-		MyString str2 = str1; // MyString str2(str1);
-		str2.Print();
-	}
+    // 복사 생성자
+    {
+        MyString str1("hi hay he hel hello llo ello el el o!");
+        MyString str2 = str1; // MyString str2(str1);
+        str2.Print();
+    }
 
-	// IsEqual()
-	{
-		MyString str3("Hello, World!");
-		cout << boolalpha;
-		cout << str3.IsEqual(MyString("Hello, World!")) << endl;
-		cout << str3.IsEqual(MyString("Hay, World!")) << endl;
-	}
+    // IsEqual()
+    {
+        MyString str3("Hello, World!");
+        cout << boolalpha;
+        cout << str3.IsEqual(MyString("Hello, World!")) << endl;
+        cout << str3.IsEqual(MyString("Hay, World!")) << endl;
+    }
 
-	// Insert()
-	{
-		MyString str4("ABCDE");
-		for (int i = 0; i <= str4.Length(); i++)
-		{
-			MyString str5 = str4.Insert(MyString("123"), i);
-			str5.Print();
-		}
-	}
+    // Insert()
+    {
+        MyString str4("ABCDE");
+        for (int i = 0; i <= str4.Length(); i++) {
+            MyString str5 = str4.Insert(MyString("123"), i);
+            str5.Print();
+        }
+    }
 
-	// Substr()
-	{
-		MyString str("ABCDEFGHIJ");
+    // Substr()
+    {
+        MyString str("ABCDEFGHIJ");
 
-		str.Substr(3, 4).Print();
-	}
+        str.Substr(3, 4).Print();
+    }
 
-	// Concat()
-	{
-		MyString str1("Hello, ");
-		MyString str2("World!");
+    // Concat()
+    {
+        MyString str1("Hello, ");
+        MyString str2("World!");
 
-		MyString str3 = str1.Concat(str2);
+        MyString str3 = str1.Concat(str2);
 
-		str3.Print();
-	}
-
-	return 0;
+        str3.Print();
+    }
+    exit(-1);
+    return 0;
 }
--- Ex0301_StringAbstractDataType/MyString.cpp
+++ Ex0301_StringAbstractDataType/MyString.cpp
@@ -3,103 +3,160 @@
 using namespace std;
 
 // 비어 있는 MyString() 생성
-MyString::MyString()
-{
-	str_ = nullptr;
-	size_ = 0;
+MyString::MyString() {
+    str_ = nullptr;
+    size_ = 0;
 }
 
 // 맨 뒤에 널 캐릭터'\0'가 들어 있는 문자열로부터 초기화
-MyString::MyString(const char* init)
-{
-	// 크기(size_) 결정
+// REVIEW
+MyString::MyString(const char *init) {
 
-	// 메모리 할당
+    for (int i = 0; init[i] != '\0'; i++) {
+        size_++;
+    }
 
-	// 데이터 복사
+    str_ = new char[size_];
+
+    memcpy(str_, init, size_);
 }
 
 // MyString의 다른 instance로부터 초기화
-MyString::MyString(const MyString& str)
-{
-	// 기본 복사 생성자는 포인터 주소만 복사하기 때문에 
-	// 소멸시 오류 발생
-	// 여기서는 새로 메모리를 할당 받아서 복사
+// NOTE: memcpy 대신에 반복문을 활용해서 복사를 함
+MyString::MyString(const MyString &str) {
+    // 기본 복사 생성자는 포인터 주소만 복사하기 때문에
+    // 소멸시 오류 발생
+    // 여기서는 새로 메모리를 할당 받아서 복사
 
+    str_ = new char[str.size_];
+    size_ = str.size_;
+
+    for (int i = 0; i < str.size_; i++) {
+        str_[i] = str.str_[i];
+    }
 }
 
-MyString::~MyString()
-{
-	// 메모리 해제
+MyString::~MyString() {
+    // 메모리 해제
+    if (str_ != nullptr) {
+        delete[] str_;
+        str_ = nullptr;
+        size_ = 0;
+    }
 }
 
-bool MyString::IsEmpty()
+bool MyString::IsEmpty() { return Length() == 0; }
+
+// REVIEW
+bool MyString::IsEqual(const MyString &str) // 편의상 참조& 사용
 {
-	return Length() == 0;
+    // 힌트: str.str_, str.size_ 가능
+    if (size_ != str.size_) {
+        return false;
+    }
+
+    for (int i = 0; i < size_; i++) {
+        if (str_[i] != str.str_[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-bool MyString::IsEqual(const MyString& str) // 편의상 참조& 사용
-{
-	// 힌트: str.str_, str.size_ 가능
+int MyString::Length() { return size_; }
 
-	return false;
-}
+// REVIEW
+void MyString::Resize(int new_size) {
+    // 메모리 재할당과 원래 갖고 있던 내용 복사
 
-int MyString::Length()
-{
-	return size_;
-}
+    if (new_size == size_) {
+        return;
+    }
 
-void MyString::Resize(int new_size)
-{
-	// 메모리 재할당과 원래 갖고 있던 내용 복사
+    MyString temp;
+    temp.str_ = new char[new_size];
+
+    memcpy(temp.str_, str_, size_);
+    delete[] str_;
+    str_ = temp.str_;
+    size_ = new_size;
 }
 
 // 인덱스 start위치의 글자부터 num개의 글자로 새로운 문자열 만들기
-MyString MyString::Substr(int start, int num)
-{
-	// 복사할 인덱스: start, start + 1, ... , start + num - 1
-	// assert(start + num - 1 < this->size_); // 문제를 단순하게 만들기 위해 가정
+MyString MyString::Substr(int start, int num) {
+    // 복사할 인덱스: start, start + 1, ... , start + num - 1
+    // assert(start + num - 1 < this->size_); // 문제를 단순하게 만들기 위해 가정
 
-	MyString temp;
+    MyString temp;
 
-	// TODO:
+    // REVIEW:
+    temp.str_ = new char[num];
+    for (int i = 0; i < num; i++) {
+        temp.str_[i] = str_[start + i];
+    }
+    temp.size_ = num;
 
-	return temp;
+    return temp;
 }
 
-MyString MyString::Concat(MyString app_str)
-{
-	MyString temp;
+MyString MyString::Concat(MyString app_str) {
+    MyString temp;
 
-	// TODO: 
+    // REVIEW:
+    temp.size_ = size_ + app_str.size_;
+    temp.str_ = new char[temp.size_];
 
-	return temp;
+    memcpy(temp.str_, str_, size_);
+    memcpy((temp.str_ + size_), app_str.str_, app_str.size_);
+
+    return temp;
 }
 
-MyString MyString::Insert(MyString t, int start)
-{
-	assert(start >= 0);
-	assert(start <= this->size_);
+MyString MyString::Insert(MyString t, int start) {
+    assert(start >= 0);
+    assert(start <= this->size_);
 
-	MyString temp;
+    MyString temp;
 
-	// TODO:
+    // REVIEW:
+    temp.size_ = size_ + t.size_;
+    temp.str_ = new char[temp.size_];
 
-	return temp;
+    for (int i = 0; i < start; i++) {
+        temp.str_[i] = str_[i];
+    }
+
+    for (int i = 0; i < t.size_; i++) {
+        temp.str_[start + i] = t.str_[i];
+    }
+
+    for (int i = start; i < size_; i++) {
+        temp.str_[t.size_ + i] = str_[i];
+    }
+
+    return temp;
 }
 
-int MyString::Find(MyString pat)
-{
-	//TODO:
-
-	return -1;
+int MyString::Find(MyString pat) {
+    // REVIEW
+    int count;
+    for (int i = 0; i < size_; i++) {
+        count = 0;
+        for (int j = 0; j < pat.size_; j++) {
+            if (str_[i + j] != pat.str_[j]) {
+                break;
+            }
+            count++;
+        }
+        if (count == pat.size_) {
+            return i;
+        }
+    }
+    return -1;
 }
 
-void MyString::Print()
-{
-	for (int i = 0; i < size_; i++)
-		cout << str_[i];
-	cout << endl;
+void MyString::Print() {
+    for (int i = 0; i < size_; i++)
+        cout << str_[i];
+    cout << endl;
 }
-
