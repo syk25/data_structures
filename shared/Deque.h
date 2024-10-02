d648ca3bb3e605ab19d2f78bf7044e15ac6486a3
syk25 <syk2245@gmail.com>
2024-10-02 19:46:35 +0900

    # 5.1 ~ 5.3 큐, 덱

--- shared/Deque.h
+++ shared/Deque.h
@@ -3,57 +3,48 @@
 #include "Queue.h"
 
 #include <cassert>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
-// Double Ended Queue (덱, dequeue와 구분)
-template<typename T>
-class Deque : public Queue<T>
+// NOTE Double Ended Queue (덱, dequeue와 구분)
+template <typename T>         // NOTE 덱은 템플릿 클래스
+class Deque : public Queue<T> // NOTE 덱은 템플릿을 사용하는 큐 상속
 {
 
-	typedef Queue<T> Base;
+    typedef Queue<T> Base; // NOTE 큐에 대해 별명 지음 -> 글자수 줄이기 위함
 
-public:
-	Deque(int capacity)
-		: Queue<T>(capacity)
-	{
-	}
+  public:
+    Deque(int capacity) : Queue<T>(capacity) {}
 
-	T& Front()
-	{
-		return Base::Front();
-	}
+    T &Front() { return Base::Front(); }
 
-	T& Back()
-	{
-		return Base::Rear();
-	}
+    T &Back() { return Base::Rear(); }
 
-	void PushFront(const T& item)
-	{
-		if (Base::IsFull())
-			Base::Resize();
+    void PushFront(const T &item) {
+        if (Base::IsFull())
+            Base::Resize();
 
-		// TODO:
-	}
+        // REVIEW
+        this->queue_[this->front_] = item;
+        this->front_ = (this->front_ + this->capacity_ - 1) % this->capacity_;
+    }
 
-	void PushBack(const T& item)
-	{
-		Base::Enqueue(item);
-	}
+    void PushBack(const T &item) { Base::Enqueue(item); }
 
-	void PopFront()
-	{
-		Base::Dequeue();
-	}
+    void PopFront() { Base::Dequeue(); }
 
-	void PopBack()
-	{
-		assert(!Base::IsEmpty());
+    void PopBack() {
+        assert(!Base::IsEmpty());
 
-		// TODO:
-	}
+        // REVIEW
+        rear_ = (rear_ + capacity_ - 1) % capacity_;
+    }
 
-private:
-	// Queue와 동일
+  private:
+    // Queue와 동일
+    // 상속받은 변수를 그대로 이름만 바꿔서 사용하고 싶은 경우
+    int &rear_ = Base::rear_;
+    int &capacity_ = Base::capacity_;
+    int &front_ = Base::front_;
+    char *&queue_ = Base::queue_;
 };
