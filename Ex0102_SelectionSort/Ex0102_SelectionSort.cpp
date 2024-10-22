1d5ac789f4da5403b0a5a532b68cbf2861dcfa3b
syk25 <syk2245@gmail.com>
2024-10-22 18:15:34 +0900

    # 1.2 선택정렬

--- Ex0102_SelectionSort/Ex0102_SelectionSort.cpp
+++ Ex0102_SelectionSort/Ex0102_SelectionSort.cpp
@@ -1,163 +1,204 @@
-﻿#include <iostream>
-#include <cassert>
+﻿#include <cassert>
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
-struct Element
-{
-	int key;
-	char value;
+struct Element {
+    int key;
+    char value;
 };
 
-bool CheckSorted(int* arr, int size)
-{
-	// TODO: 정렬 확인 함수 구현
-
-	return true;
+bool CheckSorted(int *arr, int size) {
+    // REVIEW: 정렬 확인 함수 구현
+    for (int i = 0; i < size - 1; i++) {
+        if (arr[i] > arr[i + 1]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-void Print(int* arr, int size)
-{
-	for (int i = 0; i < size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
+void Print(int *arr, int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }
 
-void Print(Element* arr, int size)
-{
-	for (int i = 0; i < size; i++)
-		cout << arr[i].key << " ";
-	cout << endl;
+void Print(Element *arr, int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i].key << " ";
+    cout << endl;
 
-	for (int i = 0; i < size; i++)
-		cout << arr[i].value << " ";
-	cout << endl;
+    for (int i = 0; i < size; i++)
+        cout << arr[i].value << " ";
+    cout << endl;
 }
 
-int main()
-{
-	// 3개 정렬
-	{
-		for (int k = 0; k < 3; k++)
-			for (int j = 0; j < 3; j++)
-				for (int i = 0; i < 3; i++) {
+int main() {
+    // 3개 정렬
+    {
+        for (int k = 0; k < 3; k++)
+            for (int j = 0; j < 3; j++)
+                for (int i = 0; i < 3; i++) {
 
-					int arr[3] = { i, j, k };
-					int size = sizeof(arr) / sizeof(arr[0]);
+                    int arr[3] = {i, j, k};
+                    int size = sizeof(arr) / sizeof(arr[0]);
 
-					for (int e = 0; e < size; e++) {
-						cout << arr[e] << " " << flush;
-					}
+                    for (int e = 0; e < size; e++) {
+                        cout << arr[e] << " " << flush;
+                    }
 
-					cout << " -> " << flush;
+                    cout << " -> " << flush;
 
-					//TODO: 정렬 해보기
+                    // REVIEW: 정렬 해보기
+                    // NOTE: 알고리즘이 제대로 작동하는 지 확인하기 위해 테스트코드를 짜는 습관도 들이자
+                    for (int i = 0; i < size - 1; i++) {
+                        for (int j = size - 1; j > 0; j--) {
+                            if (arr[j] < arr[j - 1]) {
+                                swap(arr[j], arr[j - 1]);
+                            }
+                        }
+                    }
 
-					for (int e = 0; e < size; e++) {
-						cout << arr[e] << " " << flush;
-					}
+                    for (int e = 0; e < size; e++) {
+                        cout << arr[e] << " " << flush;
+                    }
 
-					cout << boolalpha;
-					cout << CheckSorted(arr, size); // 정렬 되었나 확인
-					cout << endl;
-				}
-	}
+                    cout << boolalpha;
+                    cout << CheckSorted(arr, size); // 정렬 되었나 확인
+                    cout << endl;
+                }
+    }
 
-	return 0; // <- 실습용 임시
+    // 5개라면? 더 많다면?
+    {
+        // 8 4 2 8 3
+        // TODO: ???
 
-	// 5개라면? 더 많다면?
-	{
-		// 8 4 2 8 3
-		// TODO: ???
+        // 8 3 2 5 1 1 2 5 8 9
+        // TODO: ???
 
-		// 8 3 2 5 1 1 2 5 8 9
-		// TODO: ???
+        // 100개라면?
+    }
 
-		// 100개라면?
-	}
+    // 가장 작은 수 찾기
+    {
+        int arr[] = {8, 3, 2, 5, 1, 1, 2, 5, 8, 9}; // 임의의 숫자들, 변경 가능
+        int size = sizeof(arr) / sizeof(arr[0]);
 
-	// 가장 작은 수 찾기
-	{
-		int arr[] = { 8, 3, 2, 5, 1, 1, 2, 5, 8, 9 }; // 임의의 숫자들, 변경 가능
-		int size = sizeof(arr) / sizeof(arr[0]);
+        assert(size > 0); // size가 1이상이라고 가정
 
-		assert(size > 0); // size가 1이상이라고 가정
+        // REVIEW
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if (arr[i] > arr[j]) {
+                    swap(arr[i], arr[j]);
+                }
+            }
+        }
+        int min_number = arr[0];
+        cout << "Minimum number is " << min_number << endl;
+    }
 
-		// TODO:
+    // 가장 작은 수의 인덱스 찾기
+    {
+        int arr[] = {8, 3, 2, 5, 1, 1, 2, 5, 8, 9};
+        int size = sizeof(arr) / sizeof(arr[0]);
 
-		// cout << "Minimum number is " << min_number << endl;
-	}
+        assert(size > 0); // size가 1이상이라고 가정
 
-	// 가장 작은 수의 인덱스 찾기
-	{
-		int arr[] = { 8, 3, 2, 5, 1, 1, 2, 5, 8, 9 };
-		int size = sizeof(arr) / sizeof(arr[0]);
+        // REVIEW
+        int min_index = 0;
+        for (int i = 0; i < size; i++) {
+            min_index = arr[min_index] <= arr[i] ? min_index : i;
+        }
 
-		assert(size > 0); // size가 1이상이라고 가정
+        cout << "The index of min is " << min_index << endl;
+        cout << "Minimum number is " << arr[min_index] << endl;
+    }
 
-		// TODO:
+    // Selection Sort
+    // 힌트: swap()
+    {
+        int arr[] = {8, 3, 2, 5, 1, 1, 2, 5, 8, 9};
+        int size = sizeof(arr) / sizeof(arr[0]);
 
-		//cout << "The index of min is " << min_index << endl;
-		//cout << "Minimum number is " << arr[min_index] << endl;
-	}
+        int min_index;
+        for (int i = 0; i < size - 1; i++) {
 
-	// Selection Sort
-	// 힌트: swap()
-	{
-		int arr[] = { 8, 3, 2, 5, 1, 1, 2, 5, 8, 9 };
-		int size = sizeof(arr) / sizeof(arr[0]);
+            // REVIEW
+            min_index = i;
+            for (int j = i + 1; j < size; j++) {
+                if (arr[min_index] > arr[j]) {
+                    min_index = j;
+                }
+            }
+            swap(arr[i], arr[min_index]);
 
-		int min_index;
-		for (int i = 0; i < size - 1; i++)
-		{
+            Print(arr, size);
 
-			// TODO:
+            cout << boolalpha;
+            cout << CheckSorted(arr, size);
+            cout << endl;
+        }
+    }
 
-			Print(arr, size);
+    // 비교 횟수 세보기, 더 효율적인 방법은 없을까?
+    // https://en.wikipedia.org/wiki/Sorting_algorithm
+    {
+        ofstream ofile("log.txt");
+        for (int size = 1; size < 1000; size++) {
+            int count = 0;
+            int *arr = new int[size];
+            for (int s = 0; s < size; s++) {
+                arr[s] = size - s;
+            }
 
-			cout << boolalpha;
-			cout << CheckSorted(arr, size);
-			cout << endl;
-		}
-	}
+            int min_idx;
+            for (int i = 0; i < size - 1; i++) {
+                min_idx = i;
+                for (int j = i + 1; j < size; j++) {
+                    count++;
+                    if (arr[min_idx] > arr[j]) {
+                        min_idx = j;
+                    }
+                }
+                swap(arr[min_idx], arr[i]);
+            }
 
-	// 비교 횟수 세보기, 더 효율적인 방법은 없을까?
-	// https://en.wikipedia.org/wiki/Sorting_algorithm
-	{
-		ofstream ofile("log.txt");
-		for (int size = 1; size < 1000; size++)
-		{
-			int count = 0;
-			int* arr = new int[size];
-			for (int s = 0; s < size; s++) {
-				arr[s] = size - s;
-			}
+            // cout << size << ", " << count << endl;
+            ofile << size << ", " << count << endl;
+            // Print(arr, size);
 
-			//TODO: count ++;
+            delete[] arr;
+        }
 
-			//cout << size << ", " << count << endl;
-			ofile << size << ", " << count << endl;
-			// Print(arr, size);
+        ofile.close();
+    }
 
-			delete[] arr;
-		}
+    // [2, 2, 1]
+    // [1, 2, 2] // 첫 2가 마지막으로 이동
 
-		ofile.close();
-	}
+    // 안정성 확인(unstable)
+    {
+        Element arr[] = {{2, 'a'}, {2, 'b'}, {1, 'c'}};
+        int size = sizeof(arr) / sizeof(arr[0]);
 
-	// [2, 2, 1]
-	// [1, 2, 2] // 첫 2가 마지막으로 이동
+        Print(arr, size); // arr이 Element의 배열
 
-	// 안정성 확인(unstable)
-	{
-		Element arr[] = { {2, 'a'}, {2, 'b'}, {1, 'c'} };
-		int size = sizeof(arr) / sizeof(arr[0]);
+        // REVIEW
+        int min_index = 0;
+        for (int i = 0; i < size - 1; i++) {
+            min_index = i;
+            for (int j = i + 1; j < size; j++) {
+                if (arr[min_index].key > arr[j].key) {
+                    min_index = j;
+                }
+            }
+            swap(arr[min_index], arr[i]);
+        }
 
-		Print(arr, size); // arr이 Element의 배열
-
-		// TODO:
-
-		Print(arr, size); // arr이 Element의 배열
-	}
+        Print(arr, size); // arr이 Element의 배열
+    }
 }
