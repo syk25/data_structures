0b795c9e7d3ccb471fe36ce7d912ffae011cdc78
syk25 <syk2245@gmail.com>
2024-10-23 13:09:48 +0900

    #1.3 버블정렬

--- Ex0103_BubbleSort/Ex0103_BubbleSort.cpp
+++ Ex0103_BubbleSort/Ex0103_BubbleSort.cpp
@@ -2,41 +2,53 @@
 
 using namespace std;
 
-bool CheckSorted(int* arr, int size)
-{
-	for (int i = 0; i < size - 1; i++)
-	{
-		if (arr[i] > arr[i + 1])
-			return false;
-	}
+bool CheckSorted(int *arr, int size) {
+    for (int i = 0; i < size - 1; i++) {
+        if (arr[i] > arr[i + 1])
+            return false;
+    }
 
-	return true;
+    return true;
 }
 
-void Print(int* arr, int size)
-{
-	for (int i = 0; i < size; i++)
-		cout << arr[i] << " ";
-	cout << endl;
+void Print(int *arr, int size) {
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
 }
 
-int main()
-{
-	int arr[] = { 5, 1, 4, 2, 8 }; // 위키피디아 예시
-	// int arr[] = { 5, 4, 3, 2, 1 }; // Worst Case
-	// int arr[] = { 1, 2, 3, 5, 4 }; // Best Case
-	int n = sizeof(arr) / sizeof(arr[0]);
+int main() {
+    int arr[] = {5, 1, 4, 2, 8}; // 위키피디아 예시
+    // int arr[] = {5, 4, 3, 2, 1}; // Worst Case
+    // int arr[] = { 1, 2, 3, 5, 4 }; // Best Case
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-	Print(arr, n);
-	cout << endl;
+    Print(arr, n);
+    cout << endl;
 
-	// Bubble Sort
-	{
-		// 힌트: Print()로 진행상황 확인하면서 구현
-		// 힌트: 2중 for 루프
-		// 힌트: for 루프 범위 주의
-	}
+    // Bubble Sort
+    {
+        // 힌트: Print()로 진행상황 확인하면서 구현
+        // 힌트: 2중 for 루프
+        // 힌트: for 루프 범위 주의
 
-	// Best case
-	// Stability
+        for (int i = 0; i < n - 1; i++) {
+            bool swapped = false;
+            cout << "Pass: " << i + 1 << endl;
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr[j], arr[j + 1]);
+                    swapped = true;
+                }
+                Print(arr, n);
+            }
+            if (!swapped) {
+                break;
+            }
+            // Print(arr, n);
+        }
+    }
+
+    // Best case
+    // Stability
 }
